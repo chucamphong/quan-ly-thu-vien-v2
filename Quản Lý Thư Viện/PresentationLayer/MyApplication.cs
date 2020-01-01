c6456c5e4e2a8fa6400815e08e009e97c7e3cf29
Chu Cẩm Phong <chucamphong1999@gmail.com>
2020-01-02 00:48:07 +0700

    Tạo dự án

---
+++ Quản Lý Thư Viện/PresentationLayer/MyApplication.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using PresentationLayer.Forms;
+
+namespace PresentationLayer
+{
+    internal class MyApplication : WindowsFormsApplicationBase
+    {
+        protected override void OnCreateMainForm()
+        {
+            this.MainForm = new LoginForm();
+        }
+
+        protected override void OnCreateSplashScreen()
+        {
+            this.SplashScreen = new SplashForm();
+        }
+    }
+}

6d9cfef00e726b4844000d1bef6b47dd72400f7a
Chu Cẩm Phong <chucamphong1999@gmail.com>
2020-01-05 17:58:05 +0700

    Cập nhật giao diện

--- Quản Lý Thư Viện/PresentationLayer/Screen/UserScreen.Designer.cs
+++ Quản Lý Thư Viện/PresentationLayer/Screen/UserScreen.Designer.cs
@@ -28,12 +28,199 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.btnUpdateInfo = new Guna.UI.WinForms.GunaGradientButton();
+            this.gunaTextBox1 = new Guna.UI.WinForms.GunaTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.pnlMain.SuspendLayout();
+            this.pnlHeader.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(497, 72);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "Quản Lý Tài Khoản";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Controls.Add(this.btnUpdateInfo);
+            this.pnlMain.Controls.Add(this.gunaTextBox1);
+            this.pnlMain.Controls.Add(this.label5);
+            this.pnlMain.Controls.Add(this.lblEmail);
+            this.pnlMain.Controls.Add(this.label4);
+            this.pnlMain.Controls.Add(this.lblName);
+            this.pnlMain.Controls.Add(this.label1);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(14, 72);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(497, 217);
+            this.pnlMain.TabIndex = 8;
+            // 
+            // btnUpdateInfo
+            // 
+            this.btnUpdateInfo.Animated = true;
+            this.btnUpdateInfo.AnimationHoverSpeed = 0.07F;
+            this.btnUpdateInfo.AnimationSpeed = 0.03F;
+            this.btnUpdateInfo.BackColor = System.Drawing.Color.Transparent;
+            this.btnUpdateInfo.BaseColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(161)))), ((int)(((byte)(156)))));
+            this.btnUpdateInfo.BaseColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(247)))), ((int)(((byte)(191)))));
+            this.btnUpdateInfo.BorderColor = System.Drawing.Color.Transparent;
+            this.btnUpdateInfo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUpdateInfo.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnUpdateInfo.FocusedColor = System.Drawing.Color.Empty;
+            this.btnUpdateInfo.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdateInfo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.btnUpdateInfo.Image = null;
+            this.btnUpdateInfo.ImageSize = new System.Drawing.Size(20, 20);
+            this.btnUpdateInfo.Location = new System.Drawing.Point(97, 158);
+            this.btnUpdateInfo.Name = "btnUpdateInfo";
+            this.btnUpdateInfo.OnHoverBaseColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(170)))), ((int)(((byte)(156)))));
+            this.btnUpdateInfo.OnHoverBaseColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(254)))), ((int)(((byte)(191)))));
+            this.btnUpdateInfo.OnHoverBorderColor = System.Drawing.Color.Transparent;
+            this.btnUpdateInfo.OnHoverForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.btnUpdateInfo.OnHoverImage = null;
+            this.btnUpdateInfo.OnPressedColor = System.Drawing.Color.Transparent;
+            this.btnUpdateInfo.Radius = 3;
+            this.btnUpdateInfo.Size = new System.Drawing.Size(286, 41);
+            this.btnUpdateInfo.TabIndex = 7;
+            this.btnUpdateInfo.Text = "Cập nhật thông tin";
+            this.btnUpdateInfo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.btnUpdateInfo.TextRenderingHint = Guna.UI.WinForms.DrawingTextRenderingHint.AntiAlias;
+            this.btnUpdateInfo.Click += new System.EventHandler(this.BtnUpdateInfo_Click);
+            // 
+            // gunaTextBox1
+            // 
+            this.gunaTextBox1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaTextBox1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.gunaTextBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(43)))), ((int)(((byte)(43)))));
+            this.gunaTextBox1.BorderSize = 0;
+            this.gunaTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.gunaTextBox1.FocusedBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.gunaTextBox1.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(43)))), ((int)(((byte)(43)))));
+            this.gunaTextBox1.FocusedForeColor = System.Drawing.Color.WhiteSmoke;
+            this.gunaTextBox1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaTextBox1.Location = new System.Drawing.Point(179, 92);
+            this.gunaTextBox1.MaxLength = 50;
+            this.gunaTextBox1.Name = "gunaTextBox1";
+            this.gunaTextBox1.PasswordChar = '\0';
+            this.gunaTextBox1.Radius = 3;
+            this.gunaTextBox1.Size = new System.Drawing.Size(311, 36);
+            this.gunaTextBox1.TabIndex = 6;
+            this.gunaTextBox1.TextOffsetX = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label5.Location = new System.Drawing.Point(33, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Mật khẩu";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblEmail.Location = new System.Drawing.Point(174, 51);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(238, 25);
+            this.lblEmail.TabIndex = 4;
+            this.lblEmail.Text = "chucamphong@gmail.com";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.Location = new System.Drawing.Point(3, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Địa chỉ email";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblName.Location = new System.Drawing.Point(174, 6);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(149, 25);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Chu Cẩm Phong";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(56, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Họ tên";
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.Controls.Add(this.lblTitle);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(14, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(497, 72);
+            this.pnlHeader.TabIndex = 7;
+            // 
+            // UserScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.ClientSize = new System.Drawing.Size(525, 303);
+            this.ControlBox = false;
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.pnlHeader);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UserScreen";
+            this.Padding = new System.Windows.Forms.Padding(14, 0, 14, 14);
             this.Text = "UserScreen";
+            this.Load += new System.EventHandler(this.UserScreen_Load);
+            this.pnlMain.ResumeLayout(false);
+            this.pnlMain.PerformLayout();
+            this.pnlHeader.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label label4;
+        private Guna.UI.WinForms.GunaTextBox gunaTextBox1;
+        private Guna.UI.WinForms.GunaGradientButton btnUpdateInfo;
     }
 }
--- Quản Lý Thư Viện/PresentationLayer/Screen/UserScreen.cs
+++ Quản Lý Thư Viện/PresentationLayer/Screen/UserScreen.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using BusinessLogicLayer;
 
 namespace PresentationLayer.Screen
 {
@@ -15,6 +9,17 @@ namespace PresentationLayer.Screen
         public UserScreen()
         {
             this.InitializeComponent();
+        }
+
+        private void UserScreen_Load(object sender, EventArgs e)
+        {
+            this.lblName.Text = AuthService.User.Name;
+            this.lblEmail.Text = AuthService.User.Email;
+        }
+
+        private void BtnUpdateInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Chức năng này chưa được hỗ trợ", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
